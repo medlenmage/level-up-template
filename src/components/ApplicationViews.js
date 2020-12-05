a7fefd26ecf7413ca524bcb6a84a90d25a4ca5af
Joshua Medlen <joshua246@gmail.com>
2020-12-05 15:40:45 -0600

    profile is WIP. added profile provider and component throwing import error when npm running

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,8 @@ import { EventProvider } from './event/EventProvider'
 import { EventList } from './event/EventList'
 import { GameForm } from './game/GameForm'
 import { EventForm } from './event/EventForm'
+import { ProfileProvider } from './auth/ProfileProvider'
+import { Profile } from './auth/Profile'
 
 export const ApplicationViews = () => {
     return <>
@@ -26,6 +28,11 @@ export const ApplicationViews = () => {
                     <Route exact path="/events/new" render={props => <EventForm {...props} />} />
                 </EventProvider>
             </GameProvider>
+            <ProfileProvider>
+                <Route exact path="/profile">
+                    <Profile />
+                </Route>
+            </ProfileProvider>
         </main>
     </>
 }
